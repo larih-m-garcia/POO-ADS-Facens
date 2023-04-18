2591719afdb6cd8a8312b11343b3728553cd794f
Larissa Mara <larimara.garcia@gmail.com>
2023-04-18 19:15:06 -0300

    complete content

---
+++ src/poo/arrayList/Produto.java
@@ -0,0 +1,16 @@
+package poo.arrayList;
+
+import java.util.ArrayList;
+
+public class Produto {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
